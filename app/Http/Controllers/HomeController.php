15993a9e1b66587c931a25a5c34067e90486bd0a
Marcelo do nascimento mayer <107888086+MarceloMayer@users.noreply.github.com>
2023-09-08 22:51:23 -0300

    Base of response to home requests

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,27 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function home(){
-        return "home";
+        $data = [
+            [
+            "model"=>"Ford Fiesta",
+            "image"=>"imagemdocarro1.url",
+            "info"=>"Titanium plus 1.6 16v",
+            "price"=> 50000
+            ],
+            [
+            "model"=>"Ford Focus",
+            "image"=>"imagemdocarro2.url",
+            "info"=>"Titanium plus 2.0 16v",
+            "price"=> 58000
+            ],
+            [
+            "model"=>"Ford ka",
+            "image"=>"imagemdocarro3.url",
+            "info"=>"Titanium plus 1 16v",
+            "price"=> 35000
+            ],
+        ];
+
+        return response()->json($data);
     }
 }
